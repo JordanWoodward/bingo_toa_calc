c2976c9bf784c552b5a236e74b0e875fe57f0450
JordanWoodward <119619870+JordanWoodward@users.noreply.github.com>
2023-05-16 13:53:51 +0100

    Create points.js

---
+++ points.js
@@ -0,0 +1,14 @@
+var invo_dict = {
+	100:1,
+	150:2,
+	200:3,
+	250:4,
+	300:5
+};
+
+invocation = {}
+team_size = {}
+barrows_pieces = {}
+
+var invo_pts = invo_dict[invocation];
+var points = invo_pts + (team_size/2) + (barrows_pieces/2)
